2f1ee026253a6f87b204a8ac8daf83823ef54583
Lucas Ignacio González <63889029+lucasigna@users.noreply.github.com>
2024-01-15 16:46:10 -0300

    Update AboutMeContainer.js

--- src/components/AboutMeContainer/AboutMeContainer.js
+++ src/components/AboutMeContainer/AboutMeContainer.js
@@ -8,13 +8,14 @@ export const AboutMeContainer = () => {
       <div className="divAboutMeContainer">
           <img src={require('../../assets/foto_cv.png')} className="aboutMe_img"/>
           <h2 className="aboutMe_h2">Lucas Ignacio González</h2>
-          <p className="aboutMe_p">Tengo 23 años y estoy casi en 4to año de Ingeniería Electrónica en UTN FRA. Estoy interesado en desenvolverme en el rubro de electrónica, robótica, domótica, inmótica, IoT, entre otros. Me interesa combinar la electrónica con la programación y poder desarrollar productos con esas bases. Tengo conocimientos generales de programación y experiencia laboral como desarrollador web con React. Desde los 18 años me fui interesando en la programación y aprendí de forma autodidacta a programar en JavaScript, Python, C, entre otros. Actualmente me encuentro continuando mi aprendizaje en sistemas embebidos, IOT y Backend. <br/><br/>Aspiro en el futuro desarrollar emprendimientos tecnológicos que aporten valor a la sociedad.</p>
+          <p className="aboutMe_p">Tengo 24 años y estoy en 4to año de Ingeniería Electrónica en UTN FRA. Estoy interesado en desenvolverme en el rubro de electrónica, robótica, domótica, inmótica, IoT, entre otros. Me interesa combinar la electrónica con la programación y poder desarrollar productos con esas bases. Tengo conocimientos generales de programación y experiencia laboral como desarrollador web con React. Desde los 18 años me fui interesando en la programación y aprendí de forma autodidacta a programar en JavaScript, Python, C, entre otros. Actualmente me encuentro continuando mi aprendizaje en sistemas embebidos, IOT y Backend. <br/><br/>Aspiro en el futuro desarrollar emprendimientos tecnológicos que aporten valor a la sociedad.</p>
           <div className="divAboutMeTags">
               <p>C</p>
               <p>Python</p>
               <p>JavaScript</p>
               <p>HTML</p>
               <p>ESP32</p>
+              <p>IOT</p>
               <p>MicroPython</p>
               <p>Firebase</p>
               <p>React</p>
